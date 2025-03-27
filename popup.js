d538e1fc8ffb0f45addd1144b30afa0d6ddb3e77
tanvidiwan05 <tanvidiwan25@gmail.com>
2025-03-27 15:13:50 +0530

    popup.js file added

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.getElementById("pickColor").addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ["content.js"]
+    });
+});
